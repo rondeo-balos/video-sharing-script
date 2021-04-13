0d8a4c3d71db3c46d721bbfc7626084d7b05540a
Rondeo Balos <mr.ron669@gmail.com>
2021-04-13 20:25:27 +0800

    resources

--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,13 @@ $website = new Website($config);
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title><?php $website->title() ?> <?php $website->tagline() ?></title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		<title><?php $website->title() ?> <?php $website->tagline() ?></title>
+		<link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+		<script src="./assets/jquery.min.js"></script>
+		<script src="./assets/popper.min.js"></script>
+		<script src="./assets/bootstrap/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
